7ed4b2ec834a006caa56f9855816e5c4dc0c8d44
HuynhHuuLoc8888 <113402222+HuynhHuuLoc8888@users.noreply.github.com>
2023-05-08 13:55:48 +0700

    bo sung file

--- View/front/danhmuc_ketoanthue/dichvuquyettoanthuethunhapcanhan.php
+++ View/front/danhmuc_ketoanthue/dichvuquyettoanthuethunhapcanhan.php
@@ -11,10 +11,253 @@
             <p>
                 Cá nhân có thu nhập chịu thuế từ tiền lương, tiền công phải thực hiện quyết toán thuế thu nhập
                 cá nhân. Cá nhân có thể tự quyết toán hoặc ủy quyền cho tổ chức quyết toán thay. Nhằm hỗ trợ, đồng hành
-                cùng tổ chức, cá nhân, Đại lý thuế Việt An tư vấn hướng dẫn thủ tục <a href="#" title="quyết toán thuế">quyết toán
+                cùng tổ chức, cá nhân, Đại lý thuế Việt An tư vấn hướng dẫn thủ tục <a href="#"
+                    title="quyết toán thuế">quyết toán
                     thuế</a> cá nhân như sau:
             </p>
-            
+            <h3 class="div-left-h3" id="Xác_định_đối_tượng_phải_quyết_toán_thuế_thu_nhập_cá_nhân">
+                Xác định đối tượng phải quyết toán thuế thu nhập cá nhân
+            </h3>
+            <div class="gioithieu-full" style="margin-right:800px; margin-top:10px; margin-bottom:0;"></div>
+            <ul class="div-left-ul">
+                <li>Cá nhân cư trú có thu nhập từ tiền lương, tiền công có trách nhiệm khai quyết toán thuế nếu có số
+                    thuế phải nộp thêm.</li>
+                <li>Cá nhân có số thuế nộp thừa có đề nghị hoàn hoặc bù trừ vào kỳ khai thuế tiếp theo.</li>
+                <li>Cá nnhân cư trú có thu nhập từ tiền lương, tiền công thuộc diện xét giảm thuế do thiên tai, hỏa
+                    hoạn, tai nạn, bệnh hiểm nghèo.</li>
+                <li>Cá nhân cư trú là người nước ngoài kết thúc hợp đồng làm việc tại Việt Nam phải khai quyết toán thuế
+                    với cơ quan thuế trước khi xuất cảnh.</li>
+            </ul>
+            <h3 class="div-left-h3" id="Xác_định_thời_hạn_nộp_hồ_sơ_quyết_toán_thuế_TNCN">
+                Xác định thời hạn nộp hồ sơ quyết toán thuế TNCN
+            </h3>
+            <div class="gioithieu-full" style="margin-right:800px; margin-top:10px; margin-bottom:0;"></div>
+            <ul class="div-left-ul">
+                <li>Cá nhân có số thuế phải nộp thêm thời hạn nộp hồ sơ quyết toán thuế năm 2022 chậm nhất là ngày
+                    30/03/2023.</li>
+                <li>Cá nhân có số thuế nộp thừa, yêu cầu hoàn thuế hoặc bù trừ thuế vào kỳ khai thuế tiếp theo thì cá
+                    nhân có thể nộp hồ sơ bất cứ thời điểm nào kể từ ngày kết thúc năm tính thuế.</li>
+            </ul>
+            <h3 class="div-left-h3" id="Điều_kiện_cá_nhân_được_ủy_quyền_quyết_toán_thuế_TNCN_cho_tổ_chức">
+                Điều kiện cá nhân được ủy quyền quyết toán thuế TNCN cho tổ chức
+            </h3>
+            <div class="gioithieu-full" style="margin-right:800px; margin-top:10px; margin-bottom:0;"></div>
+            <ul class="div-left-ul">
+                <li>Cá nhân chỉ có thu nhập từ tiền lương, tiền công ký hợp đồng lao động từ 03 tháng trở lên tại một
+                    tổ chức trả thu nhập và thực tế đang làm việc tại tổ chức đó vào thời điểm ủy quyền quyết toán
+                    thuế, kể cả trường hợp cá nhân làm việc không đủ 12 tháng trong năm tại tổ chức, đồng thời có thu
+                    nhập vãng lai ở các nơi khác bình quân tháng trong năm không quá 10 triệu đồng đã được đơn vị trả
+                    thu nhập khấu trừ đủ thuế 10% mà không có yêu cầu quyết toán thuế đối với phần thu nhập này.</li>
+                <li>Trường hợp tổ chức trả thu nhập thực hiện việc tổ chức lại doanh nghiệp (chia, tách, hợp nhất, sáp
+                    nhập, chuyển đổi) và người lao động được điều chuyển từ tổ chức cũ đến tổ chức mới được hình thành
+                    sau khi tổ chức lại doanh nghiệp, nếu trong năm người lao động không có thêm thu nhập từ tiền lương,
+                    tiền công tại một nơi nào khác thì được ủy quyền quyết toán cho tổ chức mới quyết toán thuế thay,
+                    tổ chức mới phải thu lại chứng từ khấu trừ thuế TNCN do tổ chức cũ đã cấp cho người lao động để tổng
+                    hợp thu nhập, số thuế đã khấu trừ và QTT TNCN thay cho người lao động.</li>
+                <li>Trường hợp điều chuyển người lao động giữa các tổ chức trong cùng một hệ thống như: Tập đoàn, Tổng
+                    công ty, Công ty mẹ – con, Trụ sở chính và chi nhánh thì cũng được áp dụng nguyên tắc ủy quyền QTT
+                    như đối với trường hợp tổ chức lại doanh nghiệp.</li>
+            </ul>
+            <h3 class="div-left-h3" id="Xác_định_nộp_hồ_sơ_quyết_toán_thuế">
+                Xác định nộp hồ sơ quyết toán thuế
+            </h3>
+            <div class="gioithieu-full" style="margin-right:800px; margin-top:10px; margin-bottom:0;"></div>
+            <ul class="div-left-ul">
+                <li>Cá nhân có thu nhập từ tiền lương, tiền công trực tiếp khai thuế trong nămthì nơi nộp hồ sơ quyết
+                    toán thuế là cơ quan thuế nơi cá nhân nộp hồ sơ khai thuế trong năm.</li>
+                <li>Cá nhân đang tính giảm trừ gia cảnh cho bản thân tại tổ chức trả thu nhập nào thì nộp hồ sơ quyết
+                    toán thuế tại cơ quan thuế trực tiếp quản lý tổ chức trả thu nhập đó.</li>
+                <li>Trường hợp cá nhân có thay đổi nơi làm việc và tại tổ chức trả thu nhập cuối cùng có tính giảm trừ
+                    gia cảnh cho bản thân thì nộp hồ sơ quyết toán thuế tại cơ quan thuế quản lý tổ chức trả thu nhập
+                    cuối cùng.</li>
+            </ul>
+            <p>Cá nhân nộp hồ sơ quyết toán thuế tại Chi cục Thuế nơi cá nhân cư trú (nơi đăng ký thường trú hoặc tạm
+                trú) bao gồm:</p>
+            <ul class="div-left-ul">
+                <li>Cá nhân có thay đổi nơi làm việc và tại tổ chức trả thu nhập cuối cùng không tính giảm trừ gia cảnh
+                    cho bản thân.</li>
+                <li>Cá nhân chưa tính giảm trừ gia cảnh cho bản thân ở bất kỳ tổ chức trả thu nhập nào.</li>
+                <li>Cá nhân không ký hợp đồng lao động, hoặc ký hợp đồng lao động dưới 03 tháng, hoặc ký hợp đồng
+                    cung cấp dịch vụ có thu nhập tại một nơi hoặc nhiều nơi đã khấu trừ 10%.</li>
+                <li>Cá nhân trong năm có thu nhập từ tiền lương, tiền công tại một nơi hoặc nhiều nơi nhưng tại thời
+                    điểm quyết toán không làm việc tại tổ chức trả thu nhập nào.</li>
+                <li>Trường hợp cá nhân cư trú tại nhiều nơi và thuộc diện quyết toán thuế tại cơ quan thuế nơi cá nhân
+                    cư trú thì cá nhân lựa chọn một nơi cư trú để quyết toán thuế.</li>
+            </ul>
+            <h3 class="div-left-h3" id="Lập_hồ_sơ_quyết_toán_thuế_thu_nhập_cá_nhân">
+                Lập hồ sơ quyết toán thuế thu nhập cá nhân
+            </h3>
+            <div class="gioithieu-full" style="margin-right:800px; margin-top:10px; margin-bottom:0;"></div>
+            <ul class="div-left-ul">
+                <li>Tờ khai quyết toán thuế mẫu số 02/QTT-TNCN ban hành kèm theo Thông tư số 80/2021/TT-BTC.</li>
+                <li>Phụ lục mẫu số 02-1/BK-QTT-TNCN ban hành kèm theo Thông tư 80/2021/TT-BTC nếu có đăng ký giảm trừ
+                    gia cảnh cho người phụ thuộc.</li>
+                <li>Bản chụp các chúng từ chứng minh số thuế đã khấu trừ, đã tạm nộp trong năm, số thuế đã nộp ở nước
+                    ngoài (nếu có). Cá nhân cam kết chịu trách nhiệm về tính chính xác của các thông tin trên bản chụp
+                    đó. Trường hợp tổ chức trả thu nhập không cấp chứng từ khấu trừ thuế cho cá nhân do tổ chức trả thu
+                    nhập đã chấm dứt hoạt động thì cơ quan thuế căn cứ cơ sở dữ liệu của ngành thuế để xem xét xử lý hồ
+                    sơ quyết toán thuế cho cá nhân mà không bắt buộc phải có chứng từ khấu trừ thuế.</li>
+                <li>Bản chụp các hóa đơn chứng từ chứng minh khoản đóng góp vào quỹ từ thiện, quỹ nhân đạo, quỹ khuyến
+                    học trong năm tính thuế (nếu có).</li>
+                <li>Trường hợp cá nhân nhận thu nhập từ các tổ chức Quốc tế, Đại sứ quán, Lãnh sự quán và nhận thu nhập
+                    từ nước ngoài phải có tài liệu chứng minh về số tiền đã trả của đơn vị, tổ chức trả thu nhập ở nước
+                    ngoài.</li>
+                <li>Cá nhân nộp tờ khai quyết toán TNCN file XML qua hệ thống http://canhan.gdt.gov.vn/ sau đó gửi hồ sơ
+                    cứng tới nơi nộp hồ sơ quyết toán thuế.</li>
+            </ul>
+            <h3 class="div-left-h3" id="Dịch_vụ_quyết_toán_thuế_thu_nhập_cá_nhân_của_Đại_lý_thuế_Việt_An:">Dịch vụ quyết
+                toán thuế thu nhập cá nhân của Đại lý thuế Việt An:</h3>
+            <div class="gioithieu-full" style="margin-right:800px; margin-top:10px; margin-bottom:0;"></div>
+            <ul class="div-left-ul">
+                <li>Tư vấn cho khách hàng các quy định pháp luật thuế nói chung và pháp luật thuế thu nhập cá nhân nói
+                    riêng;</li>
+                <li>Tư vấn cho khách hàng các quy định và trình tự, thủ tục, hồ sơ về quyết toán thuế thu nhập cá nhân;
+                </li>
+                <li>Thực hiện quyết toán thuế thu nhập cá nhân khi có sự ủy quyền của khách hàng, bao gồm: chuẩn bị,
+                    hoàn thiện và nộp hồ sơ quyết toán tới các cơ quan nhà nước có thẩm quyền;</li>
+                <li>Thay mặt khách hàng sửa đổi, bổ sung hồ sơ, trực tiếp làm việc với cơ quan có thẩm quyền khi có yêu
+                    cầu;</li>
+                <li>Thực hiện các dịch vụ liên quan đến quyết toán thuế khác khi khách hàng có nhu cầu.</li>
+            </ul>
+            <p style="text-align: center;"><em>Quý khách hàng có nhu cầu được tư vấn về mặt pháp lý, hỗ trợ về mặt thủ
+                    tục liên quan đến quyết toán thuế thu nhập cá nhân nói riêng và các lĩnh vực thuế khác, xin vui lòng
+                    liên hệ Đại lý thuế Việt An để được tư vấn, hỗ trợ kịp thời!</em></p>
+            <div class="div-tag" style="margin-top:20px">
+                <span class="title-tag">Tags:</span>
+                <a class="a-tag" href="#" rel="tag">Quyết toán thuế</a>
+                <a class="a-tag" href="#" rel="tag">Thuế thu nhập cá nhân</a>
+            </div>
+            <div class="title-postrelated" style="margin-top:40px;">
+                <i class="icon fa-newspaper"></i>
+                <h3 class="type-postrelated">Bài viết liên quan</h3>
+                <span class="sub-related"></span>
+            </div>
+        </div>
+        <div class="btn-mucluc">
+            <i class="icon fa-bars"></i>
+            <div class="type-mucluc">Mục lục</div>
+        </div>
+        <div class="sticky-div">
+            <div class="d-right">
+                <div id="section_at" class="div-right-editor block">
+                    <div id="divright-at" class="div-right-at">
+                        <div class="bt-close">
+                            <i class="fa-times"></i>
+                        </div>
+                        <div class="div-right-title">Mục lục</div>
+                        <div class="sh-at" data-show="Hiện" data-hide="Ẩn">Ẩn</div>
+                        <ol id="at-item" type="I">
+                            <li><a href="#Xác_định_đối_tượng_phải_quyết_toán_thuế_thu_nhập_cá_nhân">Xác định đối tượng
+                                    phải quyết toán thuế thu nhập cá nhân</a></li>
+                            <li><a href="#Xác_định_thời_hạn_nộp_hồ_sơ_quyết_toán_thuế_TNCN">Xác định thời hạn nộp hồ sơ
+                                    quyết toán thuế TNCN</a></li>
+                            <li><a href="#Điều_kiện_cá_nhân_được_ủy_quyền_quyết_toán_thuế_TNCN_cho_tổ_chức">Điều kiện cá
+                                    nhân được ủy quyền quyết toán thuế TNCN cho tổ chức</a></li>
+                            <li><a href="#Xác_định_nộp_hồ_sơ_quyết_toán_thuế">Xác định nộp hồ sơ quyết toán thuế</a>
+                            </li>
+                            <li><a href="#Lập_hồ_sơ_quyết_toán_thuế_thu_nhập_cá_nhân">Lập hồ sơ quyết toán thuế thu nhập
+                                    cá nhân</a></li>
+                            <li><a href="#Dịch_vụ_quyết_toán_thuế_thu_nhập_cá_nhân_của_Đại_lý_thuế_Việt_An:">Dịch vụ
+                                    quyết toán thuế thu nhập cá nhân của Đại lý thuế Việt An:</a></li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="title-right">
+                    <h3 class="type-title-right">
+                        Dịch vụ kế toán
+                    </h3>
+                    <span class="sub-right"></span>
+                    <a href="#" title="Dịch vụ kế toán" class="link">Dịch vụ kế toán</a>
+                </div>
+                <?php
+                $CategoryRelated=new CategoryRelated();
+                $result=$CategoryRelated->showCateRelate_DVKT();
+                while($set=$result->fetch()):
+                ?>
+                <div>
+                    <div class="<?php echo $set['description']?>">
+                        <div class="title-post-related">
+                            <a class="a-post-related" href="<?php echo $set['url']?>"
+                                title="<?php echo $set['name']?>"><?php echo $set['name']?></a>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                endwhile;
+            ?>
+                <div class="title-right">
+                    <h3 class="type-title-right-2">
+                        Văn bản pháp luật
+                    </h3>
+                    <span class="sub-right"></span>
+                    <a href="#" title="Văn bản pháp luật" class="link">Văn bản pháp luật</a>
+                </div>
+                <?php
+                $CategoryRelated=new CategoryRelated();
+                $result=$CategoryRelated->showCateRelate_VBPL();
+                while($set=$result->fetch()):
+                ?>
+                <div>
+                    <div class="<?php echo $set['description']?>">
+                        <div class="title-post-related">
+                            <a class="a-post-related" href="<?php echo $set['url']?>"
+                                title="<?php echo $set['name']?>"><?php echo $set['name']?></a>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                endwhile;
+            ?>
+                <div class="title-right">
+                    <h3 class="type-title-right-2">
+                        tư vấn pháp luật
+                    </h3>
+                    <span class="sub-right"></span>
+                    <a href="#" title="tư vấn pháp luật" class="link">tư vấn pháp luật</a>
+                </div>
+                <?php
+                $CategoryRelated=new CategoryRelated();
+                $result=$CategoryRelated->showCateRelate_TVPL();
+                while($set=$result->fetch()):
+                ?>
+                <div>
+                    <div class="<?php echo $set['description']?>">
+                        <div class="title-post-related">
+                            <a class="a-post-related" href="<?php echo $set['url']?>"
+                                title="<?php echo $set['name']?>"><?php echo $set['name']?></a>
+                        </div>
+                    </div>
+                </div>
+                <?php
+                endwhile;
+            ?>
+            </div>
         </div>
     </div>
 </section>
+<script>
+jQuery(document).ready(function($) {
+    $("#at-item").toc({
+        content: "div.div-left",
+        headings: "h2,h3,h4,h5,h6"
+    });
+    $(".sh-at").click(function() {
+        $("#at-item").toggle("slide");
+    });
+    $(".sh-at").on("click", function() {
+        var second = $(this).data("show");
+        var start = $(this).data("hide");
+        if (second === $(this).text()) {
+            $(this).text(start);
+        } else {
+            $(this).text(second);
+        }
+    });
+});
+jQuery(document).on("click", ".btn-mucluc", function() {
+    jQuery("#section_at").addClass("active");
+});
+jQuery(document).on("click", ".bt-close", function() {
+    jQuery("#section_at").removeClass("active");
+});
+</script>
